92b34d95f90898ebcc8e531ec7ac478925ce796b
Srijan Sen <srijansen201@gmail.com>
2024-09-02 21:14:09 +0530

    Update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDl3U9PbDVnreYZ2TCMXTQhwLRf95Em_LA",
-  authDomain: "chatapp-ec751.firebaseapp.com",
-  projectId: "chatapp-ec751",
-  storageBucket: "chatapp-ec751.appspot.com",
-  messagingSenderId: "725161454161",
-  appId: "1:725161454161:web:33e415b11201d0d71b2670",
-  measurementId: "G-33F4BEG7MT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth(app);
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
